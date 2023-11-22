33a2f657a314c236b8184b1360a3a2db6f518876
Harvmaster <hzuccon@gmail.com>
2023-11-22 12:27:14 +1100

    Added more tests for user route

---
+++ tests/integration/testUser.ts
@@ -0,0 +1,6 @@
+export const testUser = {
+  username: 'testuser',
+  password: 'testpassword'
+}
+
+export default testUser
--- tests/integration/user.test.ts
+++ tests/integration/user.test.ts
@@ -26,6 +26,34 @@ describe('User', () => {
       console.log(response.data)
       expect(response.status).toBe(201)
     })
+
+    it('should not create a new user with the same username', async () => {
+      const response = await axios.post('http://localhost:3000/user', {
+        username: 'testuser',
+        password: 'testpassword'
+      }).catch(err =>  {
+        console.log(err.response.data)
+        expect(err.response.status).toBe(409)
+      })
+    })
+
+    it('should not create a new user when username is missing', async () => {
+      const response = await axios.post('http://localhost:3000/user', {
+        password: 'testpassword'
+      }).catch(err =>  {
+        console.log(err.response.data)
+        expect(err.response.status).toBe(400)
+      })
+    })
+
+    it('should not create a new user when password is missing', async () => {
+      const response = await axios.post('http://localhost:3000/user', {
+        username: 'testuser'
+      }).catch(err =>  {
+        console.log(err.response.data)
+        expect(err.response.status).toBe(400)
+      })
+    })
   })
   
   let access_token: string
@@ -43,6 +71,16 @@ describe('User', () => {
 
       expect(response.status).toBe(200)
     })
+
+    it('should not login a user with the wrong password', async () => {
+      const response = await axios.post('http://localhost:3000/user/login', {
+        username: 'testuser',
+        password: 'wrongpassword'
+      }).catch(err =>  {
+        console.log(err.response.data)
+        expect(err.response.status).toBe(401)
+      })
+    })
   })
 
   describe('GET /user', () => {
@@ -55,6 +93,13 @@ describe('User', () => {
       console.log(response.data)
       expect(response.status).toBe(200)
     })
+
+    it('should not get a user without a token', async () => {
+      const response = await axios.get('http://localhost:3000/user').catch(err =>  {
+        console.log(err.response.data)
+        expect(err.response.status).toBe(401)
+      })
+    })
   })
 
   describe('POST /user/refresh', () => {
@@ -66,7 +111,14 @@ describe('User', () => {
       access_token = response.data.access_token
       expect(response.status).toBe(200)
     })
+
+    it('should not refresh a user with the wrong refresh token', async () => {
+      const response = await axios.post('http://localhost:3000/user/refresh', {
+        refresh_token: 'wrongtoken'
+      }).catch(err =>  {
+        console.log(err.response.data)
+        expect(err.response.status).toBe(401)
+      })
+    })
   })
-
-
 })
